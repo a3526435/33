e1f74668b250abeae2a53846024641f3e3834bf1
a3526435 <128708729+a3526435@users.noreply.github.com>
2024-05-30 15:36:27 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SlotMachine from './components/SlotMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Slot Machine DApp</h1>
+      </header>
+      <main>
+        <SlotMachine />
+      </main>
+    </div>
+  );
+}
+
+export default App;
